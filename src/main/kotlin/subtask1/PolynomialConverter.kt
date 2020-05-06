73dd03e0b80be0223922824e716584a34a5cdeb4
Anastasia Asadcheva <58145290+anastasiaasadcheva94@users.noreply.github.com>
2020-05-06 19:02:08 +0300

    feat: add task1 solution

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -4,6 +4,42 @@ class PolynomialConverter {
 
     // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+        var i = 0
+        var y = numbers.size
+        var res = ""
+        var answer = arrayListOf<String>()
+
+        while (i < numbers.size-1) {
+                if (numbers[i] == 0) {
+                    res = ""
+                    answer.add(res)
+                    i++
+                    y--
+                }
+                if (numbers[i] == 1 || numbers[i] == -1) {
+                    res = "x"
+                    answer.add(res)
+                    val sign = if (numbers[i+1] < 0) " - " else " + "
+                    answer.add(sign)
+                    i++
+                    y--
+                }
+                if (i == numbers.size-1) {
+                    res=numbers[i].toString()
+                    answer.add(res)
+                }else {
+                        res = "${Math.abs(numbers[i])}x^${y-1}"
+                        answer.add(res)
+                        val sign = if (numbers[i+1] < 0) " - " else " + "
+                        answer.add(sign)
+                        i++
+                        y--
+                    }
+            }
+        if (numbers.isEmpty()) {
+            return null
+        }
+        return answer.joinToString ("")
     }
 }
--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -5,5 +5,6 @@ class SquareDecomposer {
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
         throw NotImplementedError("Not implemented")
+
     }
 }
